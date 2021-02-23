e052ff6cfd5f203237433281884eb1cfe9f2f971
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2021-02-23 17:31:23 -0500

    finalized DB schema

--- backend/database.sql
+++ backend/database.sql
@@ -1,39 +1,39 @@
-CREATE DATABASE groupnetwork;
-
-CREATE TABLE Users(
-'user_id' SERIAL NOT NULL PRIMARY KEY,
-'email' VARCHAR(255) NOT NULL UNIQUE KEY,
-'password' BINARY(60) NOT NULL,
-'first_name' VARCHAR(255) NOT NULL,
-'last_name' VARCHAR(255) NOT NULL,
-'gender' VARCHAR(100) NULL,
-'country_of_birth' VARCHAR(255) NULL,
-'profile_picture_url' VARCHAR(255) NULL,
-'birth_date' VARCHAR(255) NULL
+CREATE TABLE users(
+user_id BIGSERIAL NOT NULL,
+email VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+gender VARCHAR(100) NULL,
+country_of_birth VARCHAR(255) NULL,
+profile_picture_url VARCHAR(255) NULL,
+birth_date VARCHAR(255) NULL,
+PRIMARY KEY (user_id),
+UNIQUE (email)
 );
 
-
-CREATE TABLE Posts(
-'post_id' SERIAL NOT NULL PRIMARY KEY,
-'user_id' INT(20) NOT NULL FOREIGN KEY REFERENCES User('user_id')
-'caption' VARCHAR(255) NULL,
-'imageUrl' VARCHAR(255) NULL
-'date_created' DATE NOT NULL,
-'date_updated' DATE NULL
+CREATE TABLE posts(
+post_id SERIAL NOT NULL PRIMARY KEY,
+user_id INT,
+caption VARCHAR(255) NULL,
+image_url VARCHAR(255) NULL,
+date_created DATE NOT NULL,
+date_updated DATE NULL,
+CONSTRAINT fk_user
+ FOREIGN KEY (user_id)
+  REFERENCES users(user_id)
+   ON DELETE CASCADE
 );
 
-
-CREATE TABLE Comments(
-'comment_id' SERIAL NOT NULL PRIMARY KEY,
-'post_id' INT(20) NOT NULL FOREIGN KEY REFERENCES Posts('post_id'),
-'user_id' INT(20) NOT NULL FOREIGN KEY REFERENCES Users('user_id'),
-'content' TEXT NOT NULL,
-'date_created' DATE NOT NULL,
-'date_updated' DATE NULL
-);
-
-CREATE TABLE Likes(
-'user_id' INT(20) NOT NULL FOREIGN KEY REFERENCES Users('user_id'),
-'post_id' INT(20) NOT NULL FOREIGN KEY REFERENCES Posts('post_id')
-'likes' INT(20) NULL
+CREATE TABLE user_read(
+user_id INT,
+post_id INT,
+CONSTRAINT fk_user
+ FOREIGN KEY (user_id)
+  REFERENCES users(user_id)
+   ON DELETE CASCADE,
+CONSTRAINT fk_post
+ FOREIGN KEY (post_id)
+  REFERENCES posts(post_id)
+   ON DELETE CASCADE
 );
