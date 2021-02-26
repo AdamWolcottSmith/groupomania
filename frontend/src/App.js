3fed63093e4e96175f646a6e764080dd76490458
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2021-02-26 17:26:03 -0500

    inital components for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import React, { Fragment } from 'react'
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+//components
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
+
 function App() {
   return (
     <Fragment>
-
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path='/login' render={props => <Login {...props} />} />
+            <Route exact path='/register' render={props => <Register {...props} />} />
+            <Route exact path='/dashboard' render={props => <Dashboard {...props} />} />
+          </Switch>
+        </div>
+      </Router>
     </Fragment>
   );
 }
