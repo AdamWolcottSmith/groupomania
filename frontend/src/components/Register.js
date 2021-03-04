ed40c132661c60677fa6cdbc05baf664b7986615
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2021-03-04 14:30:02 -0500

    register form created

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
+
+ const [inputs, setInputs] = useState({
+  email: '',
+  password: '',
+  first_name: '',
+  last_name: '',
+ })
+
+ const { email, password, first_name, last_name } = inputs
+
+ const onChange = (e) => {
+  setInputs({ ...inputs, [e.target.name]: e.target.value })
+ }
+
+ const onSubmitForm = async (e) => {
+  e.preventDefault()
+  try {
+
+   const body = { email, password, first_name, last_name }
+
+   const response = await fetch('http://localhost:4000/auth/register', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+   })
+
+   const parseRes = await response.json()
+
+   console.log(parseRes);
+
+  } catch (error) {
+   console.error(error.message)
+  }
+ }
+
  return (
   <Fragment>
-   <h1>Register</h1>
+   <h1 className="text-center my-5">Register</h1>
+   <form onSubmit={onSubmitForm}>
+    <input type="email" name="email" placeholder="Email" className="form-control my-3" value={email} onChange={e => onChange(e)} />
+    <input type="password" name="password" placeholder="Password" className="form-control my-3" value={password} onChange={e => onChange(e)} />
+    <input type="text" name="first_name" placeholder="John" className="form-control my-3" value={first_name} onChange={e => onChange(e)} />
+    <input type="text" name="last_name" placeholder="Smith" className="form-control my-3" value={last_name} onChange={e => onChange(e)} />
+    <button className="btn btn-success btn-block">Sign Up</button>
+   </form>
   </Fragment>
  )
 }
